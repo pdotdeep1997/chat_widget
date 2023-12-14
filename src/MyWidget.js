1c89083fd062fdd7129e4d57663c8637311a537f
P Pradeep <pradeep@Ps-MacBook-Pro.local>
2023-12-13 17:35:46 -0800

    buid

--- src/MyWidget.js
+++ src/MyWidget.js
@@ -1,12 +1,15 @@
 // MyComponent.js
 import React from 'react';
 
-function MyComponent() {
+function MyWidget() {
   return (
     <div>
       <h1>Hello, World!</h1>
     </div>
   );
 }
+MyWidget.init = (config) => {
+  ReactDOM.render(<MyWidget {...config} />, document.getElementById('my-widget-container'));
+};
 
-export default MyComponent;
+export default MyWidget;
